e9a784860a425f003c9bacbab53bed11ec73f3dd
DepressedPotatok <mj.mjahan045@gmail.com>
2026-04-15 19:14:23 +0600

    about section designing 3

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -5,6 +5,7 @@ import user1 from '../../assets/images/home1-counter-img1.webp'
 import user2 from '../../assets/images/home1-counter-img2.webp'
 import user3 from '../../assets/images/home1-counter-img3.webp'
 import user4 from '../../assets/images/home1-counter-img4.webp'
+import ceoImage from '../../assets/images/home1-author-img1.webp'
 
 
 const About = () => {
@@ -86,32 +87,49 @@ const About = () => {
 
                 {/* Follow */}
                 <div>
-                    <div>
-                        <p>Follow us</p>
-                        <p>Check all the latest updates about us</p>
-                        <div>
-                            <span>DRIBBLE</span>
-                            <span>BEHANCE</span>
-                            <span>LINKEDIN</span>
-                            <span>X</span>
-                            <span>XING</span>
+                    <div className='p-5 bg-white rounded-[20px]'>
+                        <p className='dark-font-2'>Follow us</p>
+                        <p className='dark-font-3'>Check all the latest updates about us</p>
+                        <div className='about-socials-btn-container'>
+                            <a href=''>DRIBBLE</a>
+                            <a href=''>BEHANCE</a>
+                            <a href=''>LINKEDIN</a>
+                            <a href=''>X</a>
+                            <a href=''>XING</a>
                         </div>
                     </div>
-                    <div>
-                        <p>Impresssions</p>
-                        <div>
-                            <span>Solutions</span>
-                            <span>100%</span>
-                        </div>
-                        <div>
-                            <span>UI/UX</span>
-                            <span>90%</span>
-                        </div>
-                        <div>
-                            <span>Explore</span>
-                            <span>72%</span>
+                    <div className='p-5 bg-white rounded-[20px]'>
+                        <p className='dark-font-2'>Impresssions</p>
+                        <div className='flex flex-col'>
+                            <div className='bg-gray-300 px-5 py-1.25 rounded-2xl w-full'>
+                                <span>Solutions</span>
+                                <span>100%</span>
+                            </div>
+                            <div className='bg-gray-200 px-5 py-1.25 rounded-2xl  w-[90%] -mt-1.25'>
+                                <span>UI/UX</span>
+                                <span>90%</span>
+                            </div>
+                            <div className='bg-gray-100 px-5 py-1.25 rounded-2xl  w-[72%] -mt-1.25'>
+                                <span>Explore</span>
+                                <span>72%</span>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+                {/* CEO */}
+                <div className='p-5 bg-[#0a0a0a] rounded-[20px] relative'>
+                    <img src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon1.svg" alt="Awards" className='w-20 absolute right-6.25' />
+                    <img src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon2.svg" alt="Awards" className='w-20 absolute right-6.25 top-18' />
+                    <div className='ceo-img-container relative -top-10 inline-block'>
+                        <img src={ceoImage} alt="Awards" className='w-1/2' />
+                    </div>
+
+                    <div className='absolute bottom-12 px-5'>
+                        <p className='text-white text-2xl'>"At Floka, we merge strategy, creaivity, and technology to shape brands that people love."</p>
+                        <p className='text-lg dark-font-3'>Maria J. Keys / CEO</p>
+                    </div>
+
                 </div>
             </div>
 
