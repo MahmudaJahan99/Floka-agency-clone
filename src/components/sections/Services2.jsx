43e2b2934a7a55940b484311ceefd49bf35f6781
DepressedPotatok <mj.mjahan045@gmail.com>
2026-04-20 13:46:29 +0600

    services 2 content added

--- src/components/sections/Services2.jsx
+++ src/components/sections/Services2.jsx
@@ -3,7 +3,7 @@ import servicesModel from '../../assets/images/services-model.png'
 import project1 from '../../assets/images/services-project-img1.webp'
 import project2 from '../../assets/images/services-project-img2.webp'
 import project3 from '../../assets/images/services-project-img3.webp'
-import { CiStar } from 'react-icons/ci';
+import { FaStar } from 'react-icons/fa6';
 
 const Services2 = () => {
     return (
@@ -35,28 +35,31 @@ const Services2 = () => {
                         <img src={project2} />
                         <img src={project3} />
                     </div>
-                    <p className='mt-30 gradient-overlay'>More than 2k+ projects completed — each crafted to deliver real-world results for ambitious brands.</p>
+                    <p className='mt-30 text-xl font-semibold gradient-overlay'>More than 2k+ projects completed — each crafted to deliver real-world results for ambitious brands.</p>
 
                 </div>
-                <div>
-                    <div>
-                        <CiStar />
-                        <CiStar />
-                        <CiStar />
-                        <CiStar />
-                        <CiStar />
+                <div className='white-card'>
+                    <div className='flex justify-evenly text-xl'>
+                        <FaStar className='text-amber-600' />
+                        <FaStar className='text-amber-600' />
+                        <FaStar className='text-amber-600' />
+                        <FaStar className='text-amber-600' />
+                        <FaStar className='text-amber-600' />
                     </div>
-                    <p>4.9/5</p>
+
+                    <p className='text-5xl dark-font font-semibold mt-5 text-center'>4.9/5</p>
                     <div className="divider"></div>
-                    <p>We offer end-to-end creative solutions that make brands unforgettable.</p>
-                    <div>
-                        <button>+</button>
-                        <span>HIRE US NOW</span>
+                    <p className='dark-font-2 text-xl font-semibold mb-5'>We offer end-to-end creative solutions that make brands unforgettable.</p>
+                    <div className='flex items-center font-semibold'>
+                        <div className='round-btn'>
+                            <span>+</span>
+                        </div>
+                        <span className='ml-2.5'>HIRE US NOW</span>
                     </div>
                 </div>
-                <div>
-                    <span>Bases around the world</span>
-                    <span>5+</span>
+                <div className='photo-card flex justify-between items-center'>
+                    <span className='text-2xl'>Our bases around the world</span>
+                    <span className='text-5xl font-semibold mt-5 text-center'>5+</span>
                 </div>
             </div>
         </section>
