5940668e73ec9bd179d10ce4bac51b5c2da2127f
DepressedPotatok <mj.mjahan045@gmail.com>
2026-04-24 08:47:55 +0600

    designer team cards designed

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -9,34 +9,39 @@ import { FaFacebookF, FaLinkedinIn, FaXTwitter } from 'react-icons/fa6';
 
 const Team = () => {
     return (
-        <section className='section-sm'>
-            <div>
+        <section className='section-sm inter-font grid gap-5 white-card'>
+            <div className='grid gap-5'>
+                {/* Title */}
                 <h4>OUR AVENGERS</h4>
-                <p>Meet with our team member</p>
-                <div>
+                <p className='text-2xl md:text-4xl funnel-font'>Meet with our team member</p>
+                <div className='flex gap-5'>
                     <span>DESIGN TEAM</span>
                     <span>DEVELOPMENT TEAM</span>
                 </div>
-                <p>What began over coffee-fueled brainstorming sessions has grown into a thriving digital agency dedicated to helping brands stand out.</p>
+                <p className='dark-font-2'>What began over coffee-fueled brainstorming sessions has grown into a thriving digital agency dedicated to helping brands stand out.</p>
 
                 <div className='flex items-center font-semibold'>
                     <div className='round-btn'>
-                        <span>+</span>
+                        <span className='-mt-1'>+</span>
                     </div>
                     <span className='ml-2.5 dark-font-2'>JOIN US</span>
                 </div>
 
-                <div>
-                    <img src={teamFeature} />
+                <div className='w-full'>
+                    <img src={teamFeature} className='w-full rounded-[20px]' />
                 </div>
             </div>
 
-            <div>
+            <div className='grid md:grid-cols-2 gap-5 team-details-container'>
                 {/* Card 1 */}
-                <div>
-                    <div><img src={designTeam1} /></div>
-                    <p>Nicolas K. Ellington</p>
-                    <p>FOUNDER</p>
+                <div className='member-card'>
+                    <div className='member-img-container'><img src={designTeam1} /></div>
+
+                    <div className='dark-font-2 font-semibold'>
+                        <p className='dark-font-2'>Nicolas K. Ellington</p>
+                        <p className='dark-font-3'>FOUNDER</p>
+                    </div>
+
                     <div className="socials">
                         <a href="#"><FaFacebookF /></a>
                         <a href="#"><FaXTwitter /></a>
@@ -45,10 +50,14 @@ const Team = () => {
                 </div>
 
                 {/* Card 2 */}
-                <div>
-                    <div><img src={designTeam2} /></div>
-                    <p>Carlos E. Ashcroft</p>
-                    <p>CEO</p>
+                <div className='member-card'>
+                    <div className='member-img-container'><img src={designTeam2} /></div>
+
+                    <div className='dark-font-2 font-semibold'>
+                        <p className='dark-font-2'>Carlos E. Ashcroft</p>
+                        <p className='dark-font-3'>CEO</p>
+                    </div>
+
                     <div className="socials">
                         <a href="#"><FaFacebookF /></a>
                         <a href="#"><FaXTwitter /></a>
@@ -56,10 +65,14 @@ const Team = () => {
                     </div>
                 </div>
                 {/* Card 3 */}
-                <div>
-                    <div><img src={designTeam3} /></div>
-                    <p>Leonardo F. Ashton</p>
-                    <p>UX DESIGNER</p>
+                <div className='member-card'>
+                    <div className='member-img-container'><img src={designTeam3} /></div>
+
+                    <div className='dark-font-2 font-semibold'>
+                        <p className='dark-font-2'>Leonardo F. Ashton</p>
+                        <p className='dark-font-3'>UX DESIGNER</p>
+                    </div>
+
                     <div className="socials">
                         <a href="#"><FaFacebookF /></a>
                         <a href="#"><FaXTwitter /></a>
@@ -67,10 +80,14 @@ const Team = () => {
                     </div>
                 </div>
                 {/* Card 4 */}
-                <div>
-                    <div><img src={designTeam4} /></div>
-                    <p>Ricardo P. Winslow</p>
-                    <p>UI DESIGNER</p>
+                <div className='member-card'>
+                    <div className='member-img-container'><img src={designTeam4} /></div>
+
+                    <div className='dark-font-2 font-semibold'>
+                        <p className='dark-font-2'>Ricardo P. Winslow</p>
+                        <p className='dark-font-3'>UI DESIGNER</p>
+                    </div>
+
                     <div className="socials">
                         <a href="#"><FaFacebookF /></a>
                         <a href="#"><FaXTwitter /></a>
